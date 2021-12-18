8d58f82e04d39fc69fa048fed254a55978736523
Ismail hossain <ismailhsan45@gmail.com>
2021-12-18 18:57:07 +0600

    design changes

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-function validate() {
+function calculateTemp() {
     let inputTemp = parseFloat(document.getElementById('addTemp').value);
     let output = 0;
     let outOutput = document.getElementById('output');
-    var ddl = document.getElementById("cardtype");
-    var selectedValue = ddl.options[ ddl.selectedIndex ].value;
-    if (selectedValue == "selectcard") {
+    var selectedScales = document.getElementById("cardtype").value;
+    if (selectedScales == "selectcard") {
         alert("Please select a card type");
     }
     else {
-        if (selectedValue === 'F-C') {
+        if (selectedScales === 'F-C') {
             output = ((5 / 9) * (inputTemp - 32));
-            outOutput.innerText = `Output from ${inputTemp} °Fahrenheit to °Celsius is : ${output.toFixed(2)}`;
+            outOutput.innerText = `${inputTemp} °Fahrenheit to °Celsius is : ${output.toFixed(2)}`;
         }
-        else if (selectedValue === 'C-F') {
+        else if (selectedScales === 'C-F') {
             output = (((9 * inputTemp) / 5) + 32);
-            outOutput.innerText = `Output from ${inputTemp} °Celsius to °Fahrenheit is : ${output.toFixed(2)}`;
+            outOutput.innerText = `${inputTemp} °Celsius to °Fahrenheit is : ${output.toFixed(2)}`;
         }
-        else if (selectedValue === 'K-C') {
+        else if (selectedScales === 'K-C') {
             output = (inputTemp - 273.15);
-            outOutput.innerText = `Output from ${inputTemp} °Kelvin to °Celsius is : ${output.toFixed(2)}`;
+            outOutput.innerText = `${inputTemp} °Kelvin to °Celsius is : ${output.toFixed(2)}`;
         }
-        else if (selectedValue === 'C-K') {
+        else if (selectedScales === 'C-K') {
             output = (inputTemp + 273.15);
-            outOutput.innerText = `Output from ${inputTemp} °Celsius to °Kelvin is : ${output.toFixed(2)}`
+            outOutput.innerText = `${inputTemp} °Celsius to °Kelvin is : ${output.toFixed(2)}`
         }
-        else if (selectedValue === 'F-K') {
+        else if (selectedScales === 'F-K') {
             output = (((5 / 9) * (inputTemp - 32)) + 273.15)
-            outOutput.innerText = `Output from ${inputTemp} °Fahrenheit to °Kelvin is : ${output.toFixed(2)}`
+            outOutput.innerText = `${inputTemp} °Fahrenheit to °Kelvin is : ${output.toFixed(2)}`
         }
-        else if (selectedValue === 'K-F') {
+        else if (selectedScales === 'K-F') {
             output = (((9 / 5) * (inputTemp - 273.15)) + 32)
-            outOutput.innerText = `Output from ${inputTemp} °Kelvin to °Fahrenheit is : ${output.toFixed(2)}`;
+            outOutput.innerText = `${inputTemp} °Kelvin to °Fahrenheit is : ${output.toFixed(2)}`;
         }
         else {
             outOutput.innerText = 'choose any option'
         }
     }
 }
+document.getElementById("convertBtn").addEventListener('click', calculateTemp)
